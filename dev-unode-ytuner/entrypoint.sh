1c2356d25a8f3ac8218ea0c550d26bd3d2b2d764
Timo <timoadm@TimoAir.local>
2026-08-24 12:19:43 +0200

    ytuner: switch to gpopesc/ytuner image, bridge networking (ports 8095/8053)

--- dev-unode-ytuner/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-# Persistente Ordner (per ytuner.ini auf /data zeigen)
-mkdir -p /data/config /data/cache /data/db
-if [ ! -f /data/config/ytuner.ini ]; then
-  cp /app/ytuner.ini /data/config/
-fi
-cd /data/config
-exec /app/ytuner
